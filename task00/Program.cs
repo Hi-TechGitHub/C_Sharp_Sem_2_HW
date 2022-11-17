5aa521e3416acb097b2e1c384690389cc0118819
Hi-TechGitHub <116220279+Hi-TechGitHub@users.noreply.github.com>
2022-11-17 21:04:01 +0300

    task01

---
+++ task00/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Пожалуйста напишите число в диапозоне --> 100 до 999 ");
+int enter = Convert.ToInt32(Console.ReadLine());
+
+int task(int num1)
+{
+    int first = num1 / 10;
+    int second = first % 10;
+    return second;
+}
+int result = task(enter);
+Console.WriteLine(result);
---
+++ task01/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int ToThird(int num)
+{
+    while (num > 1000)
+    {
+        num = num / 10;
+    }
+    return num % 10;
+}
+if (number < 0) number = -number;
+if (number > 99)
+{
+    int result = ToThird(number);
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
